fb0347383f34be0872835800a15b784679591a9d
yort-feng <331063759@qq.com>
2014-07-13 11:39:48 +0800

    实现最基本的审计功能

--- app/models/beverage.rb
+++ app/models/beverage.rb
@@ -1,4 +1,6 @@
 class Beverage < ActiveRecord::Base
+   has_paper_trail
+
    validates :name,presence:true
    validates :price,presence:true,numericality:{greater_than_or_equal_to: 0,less_than: 100}
    validates :image,presence:true
