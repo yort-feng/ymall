d1e9f1b632763f4f7f820771f09e05e09b668edf
yort-feng <331063759@qq.com>
2014-07-13 15:57:56 +0800

    放开rails_admin里cancan,实现简单的权限控制

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  def admin?
+    self.role == "admin"
+  end
 end
